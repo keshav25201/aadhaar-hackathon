ccc1b2ef37c44188ea264fb951c92454c02b67d1
dev-kaustav <job.kaustav@gmail.com>
2021-10-30 00:55:26 +0530

    formatted

--- uid_app/add_upd/views.py
+++ uid_app/add_upd/views.py
@@ -1,26 +1,30 @@
 import json
 from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse, response
+from django.http import HttpResponse, JsonResponse, response
 from add_upd.utils import OTP
+
 # from .OTP import *
 def SignUp(request):
-    
-def getCaptcha(request): 
+    pass
+
+
+def getCaptcha(request):
     if request.method == "POST":
-        return HttpResponse(status = 401)
+        return HttpResponse(status=401)
     captcha_response_body = OTP.generate_captchar()
-    response = JsonResponse({
-        "captchaBase64String" : captcha_response_body["captchaBase64String"],
-    })
-    response.set_cookie("captchaTxnId",captcha_response_body["captchaTxnId"])
+    response = JsonResponse(
+        {
+            "captchaBase64String": captcha_response_body["captchaBase64String"],
+        }
+    )
+    response.set_cookie("captchaTxnId", captcha_response_body["captchaTxnId"])
     return response
+
+
 def getOTP(request):
-    captchaTxnId =  request.COOKIES["captchaTxnId"]
+    captchaTxnId = request.COOKIES["captchaTxnId"]
     captcha = json.loads(request.body)["captcha"]
-    otp_response_body = OTP.gen_otp(captcha,captchaTxnId)
+    otp_response_body = OTP.gen_otp(captcha, captchaTxnId)
     response = HttpResponse()
-    response.set_cookie('OTPtxnId',otp_response_body["txnId"])
+    response.set_cookie("OTPtxnId", otp_response_body["txnId"])
     return response
-
-
-     
