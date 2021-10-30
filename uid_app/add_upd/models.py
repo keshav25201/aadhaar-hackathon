5e60f35fd49244c8e3ccd1dbee589230e66b312a
keshav25201 <keshav25201@gmail.com>
2021-10-30 12:56:46 +0530

    views changes

--- uid_app/add_upd/models.py
+++ uid_app/add_upd/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class User(models.Model):
+    Name = models.CharField(max_length=50)
+    Phone_Number = models.CharField(max_length=10)
+class Request(models.Model):
+    From = models.CharField(max_length=10)
+    To  = models.CharField(max_length=10)
 # Create your models here.
--- uid_app/add_upd/views.py
+++ uid_app/add_upd/views.py
@@ -1,14 +1,17 @@
 import json
+from django.contrib.auth.models import User
+from django import http
+from add_upd.models import Request
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse, response
-from add_upd.utils import OTP
-# from .OTP import *
-def SignUp(request):
-    
+from add_upd.utils import apis
+# from .apis import *
+
+
 def getCaptcha(request): 
     if request.method == "POST":
         return HttpResponse(status = 401)
-    captcha_response_body = OTP.generate_captchar()
+    captcha_response_body = apis.generate_captchar()
     response = JsonResponse({
         "captchaBase64String" : captcha_response_body["captchaBase64String"],
     })
@@ -17,10 +20,34 @@ def getCaptcha(request):
 def getOTP(request):
     captchaTxnId =  request.COOKIES["captchaTxnId"]
     captcha = json.loads(request.body)["captcha"]
-    otp_response_body = OTP.gen_otp(captcha,captchaTxnId)
+    uid = json.loads(request.body)["uid"]
+    otp_response_body = apis.gen_otp(captcha,captchaTxnId,uid)
     response = HttpResponse()
     response.set_cookie('OTPtxnId',otp_response_body["txnId"])
     return response
 
+def SignUp(request):
+    OTPtxnId = request.COOKIES['OTPtxnId']
+    request_body = json.loads(request.body)
+    otp = request_body["otp"]
+    uid = request_body["uid"]
+    mobile = request_body["mobile"]
+    ekyc_response_body = apis.eKYC_api(otp,OTPtxnId,uid)
+    if ekyc_response_body["mobile"] == mobile:
+        user = User.objects.create_user()
+        return HttpResponse(status = 200)
+    else:
+        return HttpResponse(status = 400)
 
+def Login(request):
+    request_body = json.loads(request.body)
+    mobile = 
+    mo
+    return HttpResponse()
+def requestForSharingAddress(request):
+
+    return HttpResponse()
+
+def ResponseToRequestForAddress():
+    return HttpResponse()
      
